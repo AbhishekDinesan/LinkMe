07546a93f995f7697daaa4d1e726a6eb84966bcd
Abhishek Dinesan <abhidines04@gmail.com>
2024-08-21 14:46:41 -0400

    we're creaing calendar events, DON'T PLAY W ME!!!!

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,15 +15,19 @@ function App() {
   );
 }
 
+
 function GoogleLoginButton() {
   const googleLogin = useGoogleLogin({
     flow: 'auth-code',
+    scope: 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar.events',
     onSuccess: async (response) => {
       console.log("This is the authorization code response", response);
       const { code } = response;
       try {
         const serverResponse = await axios.post('/api/create-tokens', { code });
         console.log('Tokens:', serverResponse.data);
+        const { access_token } = serverResponse.data.tokens;
+        addEventToCalendar(access_token);
       } catch (error) {
         console.error('Error exchanging authorization code:', error.message);
       }
@@ -35,6 +39,39 @@ function GoogleLoginButton() {
   return (
     <button onClick={() => googleLogin()}>Login with Google</button>
   );
+}
+
+async function addEventToCalendar(accessToken) {
+  try {
+    const event = {
+      summary: 'Sample Event',
+      location: '123 Main St, Anytown, USA',
+      description: 'This is a sample event.',
+      start: {
+        dateTime: '2024-08-21T09:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      end: {
+        dateTime: '2024-08-21T17:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+    };
+
+    const response = await axios.post(
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+      event,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    console.log('Event created:', response.data);
+  } catch (error) {
+    console.error('Error creating event:', error.message);
+  }
 }
 
 export default App;
