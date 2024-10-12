2635cd392be8bcfdd8808d7f8dbbe17691588afc
Abhishek Dinesan <abhidines04@gmail.com>
2024-10-12 18:36:53 -0400

    fixed SQL bugs

--- back-end/models/insertTable.js
+++ back-end/models/insertTable.js
@@ -6,6 +6,12 @@ const {createTables} = require('../models/createTable')
 const insertUser = `
     INSERT INTO users (google_user_id, name, email, refresh_token, token_expires_at)
     VALUES ($1, $2, $3, $4, $5)
+    ON CONFLICT (google_user_id) DO UPDATE
+    SET 
+        name = EXCLUDED.name,
+        email = EXCLUDED.email,
+        refresh_token = EXCLUDED.refresh_token,
+        token_expires_at = EXCLUDED.token_expires_at
     RETURNING *;
 `;
 
